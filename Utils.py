1bd77f472e9920ab63e7aaa3a4844f749747a566
theSwagDude <103400297+theSwagDude@users.noreply.github.com>
2022-11-29 10:46:25 +0700

    Add files via upload

--- Utils.py
+++ Utils.py
@@ -1,4 +1,6 @@
 import pygame
+import sys
+clock = pygame.time.Clock()
 class Button:
     def __init__(self, x, y, w, l, txt, color):
         self.rect = pygame.Rect(x, y, w, l)
@@ -7,3 +9,65 @@ class Button:
     def createButton(self, x_gap, y_gap, screen):
         pygame.draw.rect(screen, self.color, self.rect, 0)
         screen.blit(self.txt, (self.rect.x + x_gap, self.rect.y + y_gap))
+#cac man hinh
+def play(screen):
+    base_font = pygame.font.Font(None, 50)
+    user_text = ''
+    input_rect = pygame.Rect(150, 150, 950, 50)
+    while True:
+        command = base_font.render("Enter an animal name:", True, (0, 0, 0))
+        play_back_button = Button(700, 420, 150, 50, base_font.render("Back", True, (0, 0, 0)), (0, 0, 255))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if play_back_button.rect.collidepoint(event.pos):
+                    main_menu(screen)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    user_text = user_text[:-1]
+                else:
+                    user_text += event.unicode
+        screen.fill((255, 255, 255))
+        screen.blit(command, (425, 100))
+        play_back_button.createButton(25, 10, screen)
+        pygame.draw.rect(screen, (0,0,255), input_rect)
+        text_surface = base_font.render(user_text, True, (255, 255, 255))
+        screen.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
+        # print(user_text)
+        pygame.display.flip()
+        clock.tick(60)
+def htp(screen):
+    while True:
+        screen.fill((255,255,255))
+        base_font = pygame.font.Font(None, 50)
+        htp_back_button = Button(700, 420, 150, 50, base_font.render("Back", True, (0, 0, 0)), (0, 0, 255))
+        htp_back_button.createButton(25,10,screen)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if htp_back_button.rect.collidepoint(event.pos):
+                    main_menu(screen)
+        pygame.display.flip()
+def main_menu(screen):
+    while True:
+        screen.fill((255,255,255))
+        base_font = pygame.font.Font(None, 50)
+        play_button = Button(100, 30, 250, 50, base_font.render("Play", True, (0, 0, 0)), (255, 255, 0))
+        htp_button = Button(800, 30, 250, 50, base_font.render("How to play", True, (0, 0, 0)), (255, 255, 0))
+        play_button.createButton(5,5, screen)
+        htp_button.createButton(22, 10, screen)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if htp_button.rect.collidepoint(event.pos):
+                    htp(screen)
+                if play_button.rect.collidepoint(event.pos):
+                    play(screen)
+        pygame.display.flip()
+
--- main.py
+++ main.py
@@ -12,46 +12,48 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
     # man hinh chinh
     screen = pygame.display.set_mode([width, height])
-    base_font = pygame.font.Font(None, 50)
-    #tao cac nut
-    htp_button = Button(800, 30, 250, 50 , base_font.render("How to play",True,(0,0,0)), (255,255,0))
-    htp_back_button = Button(700, 420, 150, 50, base_font.render("Back", True, (0, 0, 0)), (0, 0, 255))
-    user_text = ''
-    command = base_font.render("Enter an animal name:",True,(0,0,0))
-    htp_window = pygame.Rect(50, 50, 1100, 500)
-    htp_rect_clicked = False
-    htp_back_clicked = True
-    input_rect = pygame.Rect(150, 150, 950, 50)
-    color = (0,0,255)
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                 if htp_button.rect.collidepoint(event.pos):
-                    #them phan hien len huong dan
-                    htp_rect_clicked = True
-                    htp_back_clicked = False
-                 if htp_back_button.rect.collidepoint(event.pos):
-                    htp_rect_clicked = False
-                    htp_back_clicked = True
-                    print("in")
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_BACKSPACE:
-                    user_text = user_text[:-1]
-                else:
-                    user_text += event.unicode
-        if htp_back_clicked:
-            screen.fill((255, 255, 255))
-            screen.blit(command, (425, 100))
-            htp_button.createButton(22, 10, screen)
-            pygame.draw.rect(screen, color, input_rect)
-            text_surface = base_font.render(user_text, True, (255, 255, 255))
-            screen.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
-        if htp_rect_clicked:
-            pygame.draw.rect(screen, (100, 100, 100), htp_window, 0)
-            htp_back_button.createButton(25, 10, screen)
-        pygame.display.flip()
-        clock.tick(60)
-    pygame.quit()
+    main_menu(screen)
+    # base_font = pygame.font.Font(None, 50)
+    # #tao cac nut
+    # htp_button = Button(800, 30, 250, 50 , base_font.render("How to play",True,(0,0,0)), (255,255,0))
+    # htp_back_button = Button(700, 420, 150, 50, base_font.render("Back", True, (0, 0, 0)), (0, 0, 255))
+    # user_text = ''
+    # command = base_font.render("Enter an animal name:",True,(0,0,0))
+    # htp_window = pygame.Rect(50, 50, 1100, 500)
+    # htp_rect_clicked = False
+    # htp_back_clicked = True
+    # input_rect = pygame.Rect(150, 150, 950, 50)
+    # color = (0,0,255)
+    # running = True
+    # while running:
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             running = False
+    #         if event.type == pygame.MOUSEBUTTONDOWN:
+    #              if htp_button.rect.collidepoint(event.pos):
+    #                 #them phan hien len huong dan
+    #                 htp_rect_clicked = True
+    #                 htp_back_clicked = False
+    #              if htp_back_button.rect.collidepoint(event.pos):
+    #                 htp_rect_clicked = False
+    #                 htp_back_clicked = True
+    #                 print("in")
+    #         if event.type == pygame.KEYDOWN:
+    #             if event.key == pygame.K_BACKSPACE:
+    #                 user_text = user_text[:-1]
+    #             else:
+    #                 user_text += event.unicode
+    #     if htp_back_clicked:
+    #         screen.fill((255, 255, 255))
+    #         screen.blit(command, (425, 100))
+    #         htp_button.createButton(22, 10, screen)
+    #         pygame.draw.rect(screen, color, input_rect)
+    #         text_surface = base_font.render(user_text, True, (255, 255, 255))
+    #         screen.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
+    #     if htp_rect_clicked:
+    #         pygame.draw.rect(screen, (100, 100, 100), htp_window, 0)
+    #         htp_back_button.createButton(25, 10, screen)
+    #     pygame.display.flip()
+    #     print(user_text)
+    #     clock.tick(60)
+    # pygame.quit()
