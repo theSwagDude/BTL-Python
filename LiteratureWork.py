1b64ee1d1975085833ffd58c6c9a694566517a7a
theSwagDude <103400297+theSwagDude@users.noreply.github.com>
2022-11-29 16:40:05 +0700

    Add files via upload

--- LiteratureWork.py
+++ LiteratureWork.py
@@ -1,10 +1,10 @@
 class Work:
-    def __init__(self, name, grade, year, category, author_birth, author_death):
+    def __init__(self, name, year, category, author_birth, author_death, author_home):
         self.name = name
-        self.grade = grade
         self.year = year
         self.category = category
         self.author_birth = author_birth
         self.author_death = author_death
+        self.author_home = author_home
     def __str__(self):
         return "work"
