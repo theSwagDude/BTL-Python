cbd44518d86e3febe25df9e62a4655c98dce5504
AshHogan <111612126+AshHogan@users.noreply.github.com>
2022-12-01 13:23:25 +0700

    Add files via upload

---
+++ Grade_6.sql
@@ -0,0 +1,179 @@
+INSERT INTO LWorks(Grade, Id, Name, Year, Category, Author_Name, Author_Birth, Author_Death, Author_Home)
+VALUES(
+    6,
+    1,
+    "Con Rong Chau Tien",
+    0,
+    "Truyen Thuyet",
+    "Dan Gian",
+    0,
+    0,
+    "Khong ro"
+);
+INSERT INTO LWorks(Grade, Id, Name, Year, Category, Author_Name, Author_Birth, Author_Death, Author_Home)
+VALUES(
+    6,
+    2,
+    "Thanh Giong",
+    0,
+    "Truyen Thuyet",
+    "Dan Gian",
+    0,
+    0,
+    "Khong ro"
+);
+INSERT INTO LWorks(Grade, Id, Name, Year, Category, Author_Name, Author_Birth, Author_Death, Author_Home)
+VALUES(
+    6,
+    3,
+    "Son Tinh Thuy Tinh",
+    0,
+    "Truyen Thuyet",
+    "Dan Gian",
+    0,
+    0,
+    "Khong ro"
+);
+INSERT INTO LWorks(Grade, Id, Name, Year, Category, Author_Name, Author_Birth, Author_Death, Author_Home)
+VALUES(
+    6,
+    4,
+    "Thanh Sanh",
+    0,
+    "Co Tich",
+    "Dan Gian",
+    0,
+    0,
+    "Khong ro"
+);
+INSERT INTO LWorks(Grade, Id, Name, Year, Category, Author_Name, Author_Birth, Author_Death, Author_Home)
+VALUES(
+    6,
+    5,
+    "Ong lao danh ca va con ca vang",
+    0,
+    "Co Tich",
+    "Aleksandr Sergeyevich Pushkin",
+    1799,
+    1837,
+    "Nga"
+);
+INSERT INTO LWorks(Grade, Id, Name, Year, Category, Author_Name, Author_Birth, Author_Death, Author_Home)
+VALUES(
+    6,
+    6,
+    "Bai hoc duong doi dau tien",
+    1941,
+    "Truyen Hien Dai",
+    "To Hoai",
+    1920,
+    2014,
+    "Ha Noi"
+);
+INSERT INTO LWorks(Grade, Id, Name, Year, Category, Author_Name, Author_Birth, Author_Death, Author_Home)
+VALUES(
+    6,
+    7,
+    "Song nuoc Ca Mau",
+    1957,
+    "Truyen Dai",
+    "Doan Gioi",
+    1925,
+    1989,
+    "Tien Giang"
+);
+INSERT INTO LWorks(Grade, Id, Name, Year, Category, Author_Name, Author_Birth, Author_Death, Author_Home)
+VALUES(
+    6,
+    8,
+    "Buc tranh cua em gai toi",
+    1999,
+    "Truyen Ngan",
+    "Ta Duy Anh",
+    1959,
+    0,
+    "Ha Noi"
+);
+INSERT INTO LWorks(Grade, Id, Name, Year, Category, Author_Name, Author_Birth, Author_Death, Author_Home)
+VALUES(
+    6,
+    9,
+    "Vuot Thac",
+    1974,
+    "Truyen Hien Dai",
+    "Vo Quang",
+    1920,
+    2007,
+    "Quang Nam"
+);
+INSERT INTO LWorks(Grade, Id, Name, Year, Category, Author_Name, Author_Birth, Author_Death, Author_Home)
+VALUES(
+    6,
+    10,
+    "Dem nay Bac khong ngu",
+    1951,
+    "Tho 5 chu",
+    "Minh Hue",
+    1927,
+    2003,
+    "Nghe An"
+); 
+INSERT INTO LWorks(Grade, Id, Name, Year, Category, Author_Name, Author_Birth, Author_Death, Author_Home)
+VALUES(
+    6,
+    11,
+    "Luom,
+    1949,
+    "Tho 4 chu",
+    "To Huu",
+    1920,
+    2002,
+    "Thuu Thien Hue"
+);
+INSERT INTO LWorks(Grade, Id, Name, Year, Category, Author_Name, Author_Birth, Author_Death, Author_Home)
+VALUES(
+    6,
+    12,
+    "Mua",
+    1967,
+    "Tho tu do",
+    "Tran Dang Khoa",
+    1958,
+    0,
+    "Hai Duong"
+);INSERT INTO LWorks(Grade, Id, Name, Year, Category, Author_Name, Author_Birth, Author_Death, Author_Home)
+VALUES(
+    6,
+    13,
+    "Cay tre Viet Nam",
+    1955,
+    "But Ki",
+    "Thep Moi",
+    1925,
+    1991,
+    "Ha Noi"
+);
+INSERT INTO LWorks(Grade, Id, Name, Year, Category, Author_Name, Author_Birth, Author_Death, Author_Home)
+VALUES(
+    6,
+    14,
+    "Cau Long Bien chung nhan lich su",
+    0,
+    "But Ki",
+    "Thuy Lan",
+    0,
+    0,
+    "Khong ro"
+);
+INSERT INTO LWorks(Grade, Id, Name, Year, Category, Author_Name, Author_Birth, Author_Death, Author_Home)
+VALUES(
+    6,
+    5,
+    "Ong lao danh ca va con ca vang",
+    0,
+    "Co Tich",
+    "Aleksandr Sergeyevich Pushkin",
+    1799,
+    1837,
+    "Nga"
+);
