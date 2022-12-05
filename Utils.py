7de7ccba80a915ea7434bf69b1ca235a6fbdd987
theSwagDude <103400297+theSwagDude@users.noreply.github.com>
2022-12-05 17:33:03 +0700

    Add files via upload

--- Utils.py
+++ Utils.py
@@ -12,86 +12,10 @@ class Button:
         pygame.draw.rect(screen, self.color, self.rect, 0)
         screen.blit(self.txt, (self.rect.x + x_gap, self.rect.y + y_gap))
 
-# def displayGuess(work):
-#     guesss_rect = pygame.Rect(350,150,950,50)
-#     base_font = pygame.font.Font(None, 50)
-
-# #cac man hinh
-# def play(screen):
-#     #cac bai
-#     works = []
-#     #cac bai da doan
-#     guesses = []
-#     #vi du
-#     works.append(Work("Co To", 1976, 1910, 1987, "Ha Noi"))
-#
-#     base_font = pygame.font.Font(None, 50)
-#     user_text = ''
-#     input_rect = pygame.Rect(150, 150, 950, 50)
-#     while True:
-#         command = base_font.render("Enter an animal name:", True, (0, 0, 0))
-#         play_back_button = Button(800, 30, 250, 50, base_font.render("Back", True, (0, 0, 0)), (0, 0, 255))
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 pygame.quit()
-#                 sys.exit()
-#             if event.type == pygame.MOUSEBUTTONDOWN:
-#                 if play_back_button.rect.collidepoint(event.pos):
-#                     main_menu(screen)
-#             if event.type == pygame.KEYDOWN:
-#                 if event.key == pygame.K_RETURN:
-#                     guesses.append(user_text)
-#                     user_text = ''
-#                 elif event.key == pygame.K_BACKSPACE:
-#                     user_text = user_text[:-1]
-#                 else:
-#                     user_text += event.unicode
-#         screen.fill((255, 255, 255))
-#         screen.blit(command, (425, 100))
-#         play_back_button.createButton(25, 10, screen)
-#
-#         #xu li bai doan
-#         for guess in guesses:
-#             for work in works:
-#                 if(guess == work.name):
-#                     displayGuess(work)
-#
-#         pygame.draw.rect(screen, (0,0,255), input_rect)
-#         text_surface = base_font.render(user_text, True, (255, 255, 255))
-#         screen.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
-#         # print(user_text)
-#         pygame.display.flip()
-#         clock.tick(60)
-# def htp(screen):
-#     while True:
-#         screen.fill((255,255,255))
-#         base_font = pygame.font.Font(None, 50)
-#         htp_back_button = Button(700, 420, 150, 50, base_font.render("Back", True, (0, 0, 0)), (0, 0, 255))
-#         htp_back_button.createButton(25,10,screen)
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 pygame.quit()
-#                 sys.exit()
-#             if event.type == pygame.MOUSEBUTTONDOWN:
-#                 if htp_back_button.rect.collidepoint(event.pos):
-#                     main_menu(screen)
-#         pygame.display.flip()
-# def main_menu(screen):
-#     while True:
-#         screen.fill((255,255,255))
-#         base_font = pygame.font.Font(None, 50)
-#         play_button = Button(100, 30, 250, 50, base_font.render("Play", True, (0, 0, 0)), (255, 255, 0))
-#         htp_button = Button(800, 30, 250, 50, base_font.render("How to play", True, (0, 0, 0)), (255, 255, 0))
-#         play_button.createButton(5,5, screen)
-#         htp_button.createButton(22, 10, screen)
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 pygame.quit()
-#                 sys.exit()
-#             if event.type == pygame.MOUSEBUTTONDOWN:
-#                 if htp_button.rect.collidepoint(event.pos):
-#                     htp(screen)
-#                 if play_button.rect.collidepoint(event.pos):
-#                     play(screen)
-#         pygame.display.flip()
-
+#xem chu dau cua 2 string co giong nhau ko
+def checkString(s1, s2):
+    l1 = s1.split()
+    l2 = s2.split()
+    if(l1[0].casefold() == l2[0].casefold()):
+        return True
+    return False
--- main.py
+++ main.py
@@ -1,41 +1,41 @@
+import sys
 import time
 import pygame
 import random
 from Utils import *
 from LiteratureWork import Work
-# neu ten dai qua, chuyen font
-# neu the loai co mot chu giong, de mau vang
-# them nut quit
+
 from mySQLConn import connect
+
 if __name__ == '__main__':
     pygame.init()
     pygame.font.init()
-    # dat ten chuong trinh
-    pygame.display.set_caption("Anidle")
     # do dai, rong window
     width = 1200
     height = 750
-    # clock = pygame.time.Clock()
     # man hinh chinh
     screen = pygame.display.set_mode([width, height])
     base_font = pygame.font.SysFont(None, 30)
+    smaller_font = pygame.font.SysFont(None, 18)
+    smaller_cfont = pygame.font.SysFont(None, 24)
     # cac bai
     works = connect()
     # cac bai da doan
     guesses = []
-    # vi du
-    # works.append(Work("Co To", 1976, "Ki", 1910, 1987, "Ha Noi"))
-    # works.append(Work("Luom", 1975, "Ki", 1910, 1987, "Ha Noi"))
-    # works.append(Work("lhi Pheo", 1976, "Tho", 1920, 1977, "Bac Ninh"))
+
     # lua chon dap an dung
     # winner = works[random.randint(0, len(works) - 1)]
-    winner = works[5]
+    winner = works[15]
     suggestions = []
+
     background = pygame.image.load("background.jpg")
     background = pygame.transform.scale(background,(1200,750))
     arrow = pygame.image.load("uparrow.png")
     up_arrow = pygame.transform.scale(arrow, (30,30))
     down_arrow = pygame.transform.flip(up_arrow, True, True)
+    htp_img = pygame.image.load("howtoplay.png")
+    htp_img = pygame.transform.scale(htp_img, (1200,750))
+
     # cac man hinh
     def play():
         user_text = ''
@@ -86,7 +86,7 @@ if __name__ == '__main__':
             pygame.draw.rect(screen, (255,255,255), input_rect)
             text_surface = base_font.render(user_text, True, (0,0,0))
             screen.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))
-            # print(user_text)
+
             #du doan dap an
             suggestions.clear()
             if user_text:
@@ -127,8 +127,8 @@ if __name__ == '__main__':
             screen.blit(base_font.render(str(work.author_death), True, (255, 255, 255)), (850, 100))
             screen.blit(base_font.render(str(work.author_home), True, (255, 255, 255)), (1000, 100))
 
-            play_again_button = Button(700, 420, 200, 50, base_font.render("Play Again", True, (0, 0, 0)), (169, 252, 3))
-            play_again_button.createButton(25, 10, screen)
+            play_again_button = Button(720, 440, 200, 50, base_font.render("Choi lan nua", True, (0, 0, 0)), (169, 252, 3))
+            play_again_button.createButton(35, 15, screen)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -146,8 +146,9 @@ if __name__ == '__main__':
         while True:
             screen.fill((255, 255, 255))
             screen.blit(background, (0, 0))
-            htp_back_button = Button(700, 420, 150, 50, base_font.render("Back", True, (0, 0, 0)), (0, 0, 255))
-            htp_back_button.createButton(25, 10, screen)
+            screen.blit(htp_img, (0,0))
+            htp_back_button = Button(975, 30, 125, 50, base_font.render("Tro Ve", True, (0, 0, 0)), (169, 252, 3))
+            htp_back_button.createButton(30, 15, screen)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -157,7 +158,6 @@ if __name__ == '__main__':
                         main_menu()
             pygame.display.flip()
 
-
     def main_menu():
         guesses.clear()
         suggestions.clear()
@@ -166,8 +166,10 @@ if __name__ == '__main__':
             screen.blit(background,(0, 0))
             play_button = Button(475, 250, 250, 50, base_font.render("Choi", True,  (0, 0, 0)), (169, 252, 3))
             htp_button = Button(475, 325, 250, 50, base_font.render("Cach choi", True, (0, 0, 0)), (169, 252, 3))
+            quit_button = Button(475, 400, 250, 50, base_font.render("Thoat", True, (0, 0, 0)), (169, 252, 3))
             play_button.createButton(100, 15, screen)
-            htp_button.createButton(55, 15, screen)
+            htp_button.createButton(80, 15, screen)
+            quit_button.createButton(100, 15, screen)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -177,8 +179,12 @@ if __name__ == '__main__':
                         htp()
                     if play_button.rect.collidepoint(event.pos):
                         play()
+                    if quit_button.rect.collidepoint(event.pos):
+                        pygame.quit()
+                        sys.exit()
             pygame.display.flip()
 
+    #hien thi nhung bai da doan
     def displayGuess(work, y):
         #display o tren
         screen.blit(base_font.render(u"Ten", True, (255,255,255)), (100, 170))
@@ -190,6 +196,7 @@ if __name__ == '__main__':
         screen.blit(base_font.render(u"Que", True, (255,255,255)), (1000, 170))
         guesss_rect = pygame.Rect(25, y, 1100, 50)
         pygame.draw.rect(screen, (50, 50, 50), guesss_rect)
+
         # so sanh
         if work.year == winner.year:
             pygame.draw.rect(screen, (0,255,0), pygame.Rect(275,y, 140, 50))
@@ -204,31 +211,33 @@ if __name__ == '__main__':
         if work.grade == winner.grade:
             pygame.draw.rect(screen, (0,255,0), pygame.Rect(450,y, 70, 50))
         else:
-            if abs(work.year - winner.year) <= 100:
-                pygame.draw.rect(screen, (240, 252, 3), pygame.Rect(450, y, 100, 50))
-            if work.year - winner.year < 0:
+            if abs(work.grade - winner.grade) <= 1:
+                pygame.draw.rect(screen, (240, 252, 3), pygame.Rect(450, y,70, 50))
+            if work.grade - winner.grade < 0:
                 screen.blit(up_arrow, (490, y))
             else:
                 screen.blit(down_arrow, (490, y))
 
         if work.category == winner.category:
-            pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(500, y, 140, 50))
+            pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(525, y, 145, 50))
+        elif checkString(work.category, winner.category):
+            pygame.draw.rect(screen, (240, 252, 3), pygame.Rect(525, y, 145, 50))
 
         if work.author_birth == winner.author_birth:
-            pygame.draw.rect(screen, (0,255,0), pygame.Rect(650,y, 140, 50))
+            pygame.draw.rect(screen, (0,255,0), pygame.Rect(680,y, 110, 50))
         else:
             if abs(work.author_birth - winner.author_birth) <= 100:
-                pygame.draw.rect(screen, (240, 252, 3), pygame.Rect(650, y, 140, 50))
+                pygame.draw.rect(screen, (240, 252, 3), pygame.Rect(680, y, 110, 50))
             if work.author_birth - winner.author_birth < 0:
                 screen.blit(up_arrow, (760, y))
             else:
                 screen.blit(down_arrow, (760, y))
 
         if work.author_death == winner.author_death:
-            pygame.draw.rect(screen, (0,255,0), pygame.Rect(800,y, 140, 50))
+            pygame.draw.rect(screen, (0,255,0), pygame.Rect(810,y, 140, 50))
         else:
             if abs(work.author_death - winner.author_death) <= 100:
-                pygame.draw.rect(screen, (240, 252, 3), pygame.Rect(800, y, 140, 50))
+                pygame.draw.rect(screen, (240, 252, 3), pygame.Rect(810, y, 140, 50))
             if work.author_death - winner.author_death < 0:
                 screen.blit(up_arrow, (910, y))
             else:
@@ -238,14 +247,18 @@ if __name__ == '__main__':
             pygame.draw.rect(screen, (0,255,0), pygame.Rect(950,y, 200, 50))
 
         #cac dap an
-
-        screen.blit(base_font.render(work.name, True, (255,255,255)), (50, y + 15))
-        screen.blit(base_font.render(str(work.year), True, (255,255,255)), (300, y + 15))
+        if len(work.name) < 19:
+            screen.blit(base_font.render(work.name, True, (255,255,255)), (50, y + 15))
+        else:
+            screen.blit(smaller_font.render(work.name, True, (255, 255, 255)), (50, y + 15))
+        screen.blit(base_font.render(str(work.year), True, (255,255,255)), (310, y + 15))
         screen.blit(base_font.render(str(work.grade), True, (255, 255, 255)), (475, y + 15))
-        screen.blit(base_font.render(work.category, True, (255,255,255)), (550, y+15))
+        if len(work.category) < 11:
+            screen.blit(base_font.render(work.category, True, (255,255,255)), (550, y+15))
+        else:
+            screen.blit(smaller_cfont.render(work.category, True, (255, 255, 255)), (550, y + 15))
         screen.blit(base_font.render(str(work.author_birth), True, (255,255,255)), (700, y+15))
         screen.blit(base_font.render(str(work.author_death), True, (255,255,255)), (850, y+15))
         screen.blit(base_font.render(str(work.author_home), True, (255,255,255)), (1000, y+15))
-
 
     main_menu()
